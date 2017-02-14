0b46572994cb1a7f3354e52c15ec46885f117877
Ryan Linnit <ryan.linnit@outlook.com>
2017-02-14 23:57:53 +0100

    Stylin

--- terminal.php
+++ terminal.php
@@ -2,13 +2,12 @@
 <html lang="en">
 	<head>
 		<title>SJSTerm</title>
-		<link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css" crossorigin="anonymous">
 
 		<script src="https://code.jquery.com/jquery-2.2.3.min.js"
 				  integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo="
 				  crossorigin="anonymous"></script>
 		<link href='https://fonts.googleapis.com/css?family=Cutive+Mono' rel='stylesheet' type='text/css'>
-		<link href='/style.css' rel='stylesheet' type='text/css'>
+		<link href='/style.css?time=<?php echo time();?>' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 		<div id="inputContainer">
@@ -19,13 +18,9 @@
 				<h3 class="text-muted">SJSTerm</h3>
 			</div>
 
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="term">
-					</div>
-				</div>
+			<div class="term">
 			</div>
 		</div>
 	</body>
-	<script src="/terminal.js"></script>
+	<script src="/terminal.js?time=<?php echo time();?>"></script>
 </html>
