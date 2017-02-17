a2eb63f7fc5b12b3a615dacd74057e5355879da1
Ryan Linnit <ryan.linnit@outlook.com>
2017-02-17 23:40:04 +0100

    xxx

--- terminal.js
+++ terminal.js
@@ -1,7 +1,7 @@
 
 $('.active').focus();
 
-var cursorPosition = 0;
+var cursorPosition = [0,0];
 var user = "ryan";
 var hostname = "tim";
 var exitStatus = 0;
@@ -13,40 +13,40 @@ var filesystem = {
 			"redhat-release" : "CentOS Linux release 7.3.1611 (Core)"
 		},
 		"home" : {
-			"testfile" : "This is a test file",
+			"testfile" : "This is a test file\nblahblah",
 			"ryan" : {
-				"pgpkey" : "-----BEGIN PGP PUBLIC KEY BLOCK----- <br>\
-xsBNBFiOLzMBCADOff8IZQx/SS4ASBTKOZBmfo8IScElprMC6BjFvVSKIG+J<br>\
-pd4LlDPDXOtJ+8ZijqMqkyuuqURFwRbpDR1sBTUk3E7lKmLTiPZjubEoQPCO<br>\
-UC6p3r0yl6Vf891o6yyAl0PZMT95cPiKEuHuBbAYN08qNmZ6JScsVvfSBraV<br>\
-ZeJW2byTfnaKwe4CatnkfAbnSY6HaN0A7DRNny4uzTAETj18Ht2abBVuTVAC<br>\
-2fBWmx8q4gS6ZsVc8q5eDhGdmHSAc5Vg5GkmdsolWMYWdc25b8fJPPJDcOUd<br>\
-FdLwC3fdvez6RKUrrA04MsG54Bs+50CmgJRkVwSZWAbQ9f9hZK3jdHtPABEB<br>\
-AAHNLWxpbm5pdEBwcm90b25tYWlsLmNvbSA8bGlubml0QHByb3Rvbm1haWwu<br>\
-Y29tPsLAdQQQAQgAKQUCWI4vNAYLCQcIAwIJEE5R9o8K1Eq/BBUIAgoDFgIB<br>\
-AhkBAhsDAh4BAABJ9Qf7BDn3MS4HR7esPEfiibGHjUFHpPa/lsa1+ZJaPKiu<br>\
-6CNGN1OxIUMmEiOn8s8FDQplkgnMaqPOVkGFgffevjisiqDwUVBwF+LIkxev<br>\
-ysr3XfysP30N3O143qfJBNmg4imY653hMi2tyrrB3NPlRfUa7Q0UHRtwhrcO<br>\
-qpqn9bOa021SUnTLY4ILj+urMpEWL87ImeE4qCld1aay/SUQHTUkL7rtryuO<br>\
-Nm6ZG1SRN2euJvX16s4XEVp52Hy7JPLzYOwQo4raW2VZyp+h5+7GVEkvydo7<br>\
-FJfjSOGQl9mK9gFEaojrEERY69Hc4McXDg+DpLlpYa7kHaEOHkIDAwWYKqHj<br>\
-DM7ATQRYji8zAQgAoScG4OwfEezF8llVju1zJwQNtN8umuH/4GqjQ/Z7buxh<br>\
-swxdv5itVMVP4nkl+ukxdhK57VKySWtJ4Ut0ExNmtAzEhI1kg3j7U9/+vScJ<br>\
-Gfx9ihkoDQLC97YEIQb5BwkhaEcuivPV+ywpD4hFc/nIAeqI+xuuudjguar7<br>\
-r9s2WBhYTS9t1+SCdFEiIPLUAu24KdOcJrAfXiK0YeeoQ/cX/cMH/3tIpiks<br>\
-Z0w0d4YebflGCB7V+epPeE+d0SdEnOON9XzgC5h0RIO4rAEpQGQZ/90bCuhG<br>\
-jWfOhtzBPPSs+JU6UyzwIvIGGuGqXipeagDYKyQZfcAaw6oHEvnQyNSavQAR<br>\
-AQABwsBfBBgBCAATBQJYji80CRBOUfaPCtRKvwIbDAAAGmEIAKCYMLyXO3HZ<br>\
-wMuvyWXnZ4311eoqWOZvFmm05sFM2eitDvRFSF+T7RkxGHSf0bVvt5nhpizB<br>\
-TYMft8h1Qb0vRtVU09pPsbIPeeKx0DQVmjqBCdsisSBdXD1Gxe++AvpIfMRm<br>\
-nj+1osGXTrmNb6dqJPTnywfBsNrdI9hUp1yvxLgk+61vQDnKAqKnc8nvd4rr<br>\
-5shrI/99IdF7ku8aFZNzZgfW8y2A/wA7slfUw8yuqbV++2rMXqxcK+2MhORX<br>\
-DNapP3PG5FdhCkWrsIzpd/Q6HRbEC087cOZ/9igQOJdztstHjCbhlME6J1Gs<br>\
-ivdE5tDRduEYPMjfK+p3VHoNivZS7XQ=<br>\
-<br>\
-=PL6b<br>\
-<br>\
------END PGP PUBLIC KEY BLOCK-----<br>\
+				"pgpkey" : "-----BEGIN PGP PUBLIC KEY BLOCK-----\n\
+xsBNBFiOLzMBCADOff8IZQx/SS4ASBTKOZBmfo8IScElprMC6BjFvVSKIG+J\n\
+pd4LlDPDXOtJ+8ZijqMqkyuuqURFwRbpDR1sBTUk3E7lKmLTiPZjubEoQPCO\n\
+UC6p3r0yl6Vf891o6yyAl0PZMT95cPiKEuHuBbAYN08qNmZ6JScsVvfSBraV\n\
+ZeJW2byTfnaKwe4CatnkfAbnSY6HaN0A7DRNny4uzTAETj18Ht2abBVuTVAC\n\
+2fBWmx8q4gS6ZsVc8q5eDhGdmHSAc5Vg5GkmdsolWMYWdc25b8fJPPJDcOUd\n\
+FdLwC3fdvez6RKUrrA04MsG54Bs+50CmgJRkVwSZWAbQ9f9hZK3jdHtPABEB\n\
+AAHNLWxpbm5pdEBwcm90b25tYWlsLmNvbSA8bGlubml0QHByb3Rvbm1haWwu\n\
+Y29tPsLAdQQQAQgAKQUCWI4vNAYLCQcIAwIJEE5R9o8K1Eq/BBUIAgoDFgIB\n\
+AhkBAhsDAh4BAABJ9Qf7BDn3MS4HR7esPEfiibGHjUFHpPa/lsa1+ZJaPKiu\n\
+6CNGN1OxIUMmEiOn8s8FDQplkgnMaqPOVkGFgffevjisiqDwUVBwF+LIkxev\n\
+ysr3XfysP30N3O143qfJBNmg4imY653hMi2tyrrB3NPlRfUa7Q0UHRtwhrcO\n\
+qpqn9bOa021SUnTLY4ILj+urMpEWL87ImeE4qCld1aay/SUQHTUkL7rtryuO\n\
+Nm6ZG1SRN2euJvX16s4XEVp52Hy7JPLzYOwQo4raW2VZyp+h5+7GVEkvydo7\n\
+FJfjSOGQl9mK9gFEaojrEERY69Hc4McXDg+DpLlpYa7kHaEOHkIDAwWYKqHj\n\
+DM7ATQRYji8zAQgAoScG4OwfEezF8llVju1zJwQNtN8umuH/4GqjQ/Z7buxh\n\
+swxdv5itVMVP4nkl+ukxdhK57VKySWtJ4Ut0ExNmtAzEhI1kg3j7U9/+vScJ\n\
+Gfx9ihkoDQLC97YEIQb5BwkhaEcuivPV+ywpD4hFc/nIAeqI+xuuudjguar7\n\
+r9s2WBhYTS9t1+SCdFEiIPLUAu24KdOcJrAfXiK0YeeoQ/cX/cMH/3tIpiks\n\
+Z0w0d4YebflGCB7V+epPeE+d0SdEnOON9XzgC5h0RIO4rAEpQGQZ/90bCuhG\n\
+jWfOhtzBPPSs+JU6UyzwIvIGGuGqXipeagDYKyQZfcAaw6oHEvnQyNSavQAR\n\
+AQABwsBfBBgBCAATBQJYji80CRBOUfaPCtRKvwIbDAAAGmEIAKCYMLyXO3HZ\n\
+wMuvyWXnZ4311eoqWOZvFmm05sFM2eitDvRFSF+T7RkxGHSf0bVvt5nhpizB\n\
+TYMft8h1Qb0vRtVU09pPsbIPeeKx0DQVmjqBCdsisSBdXD1Gxe++AvpIfMRm\n\
+nj+1osGXTrmNb6dqJPTnywfBsNrdI9hUp1yvxLgk+61vQDnKAqKnc8nvd4rr\n\
+5shrI/99IdF7ku8aFZNzZgfW8y2A/wA7slfUw8yuqbV++2rMXqxcK+2MhORX\n\
+DNapP3PG5FdhCkWrsIzpd/Q6HRbEC087cOZ/9igQOJdztstHjCbhlME6J1Gs\n\
+ivdE5tDRduEYPMjfK+p3VHoNivZS7XQ=\n\
+\n\
+=PL6b\n\
+\n\
+-----END PGP PUBLIC KEY BLOCK-----\n\
 "
 			}
 		},
@@ -72,10 +72,9 @@ var commands = {
 var cwd = users[user][0];
 
 var commandHistory = [];
-var lineNumber = 0;
 
 $(function(){
-	$('.term').append('<div>['+user+'@'+hostname+' ~]# <span class="active_command"></span><span class="cursor">_</span></div>');
+	$('.term').append('<code>['+user+'@'+hostname+' ~]# <span class="active_command"></span><span class="cursor">_</span></code>');
 
 	$(document).on('click', function() {
 		$('.active').focus();
@@ -99,10 +98,10 @@ $(function(){
 				if ($('.active').val().length !== 0) {
 					output = parseCommand($('.active').val());
 					commandHistory.push($('.active').val());
-					lineNumber = commandHistory.length;
+					cursorPosition[1] = commandHistory.length;
 				
 					if (output) {
-						$('.term').append('<div>'+output+'</div>');
+						$('.term').append('<code style="white-space: pre-wrap;">'+output+'</code>');
 					}
 				}
 
@@ -110,7 +109,7 @@ $(function(){
 					pscwd = "~";
 				}
 
-				$('.term').append('<div>['+user+'@'+hostname+' ' +pscwd+ ']# <span class="active_command"></span><span class="cursor">_</span></div>');
+				$('.term').append('<code>['+user+'@'+hostname+' ' +pscwd+ ']# <span class="active_command"></span><span class="cursor">_</span></code>');
 				$('.term').scrollTop($('.term')[0].scrollHeight);
 
 				$('.active').val('');
@@ -122,8 +121,8 @@ $(function(){
 			case 38:
 				// Up arrow
 			
-				if (lineNumber > 0) {
-					stuff = commandHistory[--lineNumber];
+				if (cursorPosition[1] > 0) {
+					stuff = commandHistory[--cursorPosition[1]];
 					console.log(stuff);
 					//movement = stuff.length - $('.active').val().length;
 
@@ -150,7 +149,7 @@ $(function(){
 			case 8:
 				// Backspace
 				setTimeout(function(){
-					cursorPosition--;
+					cursorPosition[0]--;
 					cursorMove(0);
 				}, 1);
 				break;
@@ -162,7 +161,7 @@ $(function(){
 			default:
 				if (event.keyCode === 0 || (event.keyCode >= 32 && event.keyCode <= 222)) {
 					setTimeout(function(){
-						cursorPosition++;
+						cursorPosition[0]++;
 						cursorMove(0);
 					}, 1);
 				}
@@ -187,31 +186,56 @@ function cursorMove(direction) {
 	//	}, 1000);
 	//}
 
-	//command = command.replace("<", "&lt;");
-	//command = command.replace(">", "&gt;");
+	if (/</.test(command)) {
+		command = command.replace("<", "&lt;");
+	} else if (/>/.test(command)) {
+		command = command.replace(">", "&gt;");
+	}
+
 	length = command.length;
 
-	cursorPosition = cursorPosition + direction;
+	cursorPosition[0] = cursorPosition[0] + direction;
 
-	if (cursorPosition < 0) {
-		cursorPosition = 0;
+	if (cursorPosition[0] < 0) {
+		cursorPosition[0] = 0;
 		return false;
 	}
 
-	if (cursorPosition > length) {
-		cursorPosition = length;
+	if (cursorPosition[0] > length) {
+		cursorPosition[0] = length;
 		return false;
 	}
 
-	if (command.substr(cursorPosition, 1).length == 1) {
+	if (command.substr(cursorPosition[0], 1).length == 1) {
 		$('.cursor').hide();
 	} else {
 		$('.cursor').show();
 	}
 
-	newHtml = command.substr(0, cursorPosition) + '<span class="onCursor">' + command.substr(cursorPosition, 1)+ '</span>' + command.substr(cursorPosition +1);
+	commandBeforeCurs = command.substr(0, cursorPosition[0]);
+	commandInCursor = command.substr(cursorPosition[0], 1).replace("&lt;", "<");
+	commandAfterCurs = command.substr(cursorPosition[0] +1);
+
+	fuckCharacter(command, cursorPosition[0]);
+	
+	//             - - - -
+	// 0 1 2 3 4 5 6 7 8 9 10
+	// T h e b f g & l t ; a
+	//             | - (i, i+3)
+	//               | - (i-1, i+2)
+	//                 | - (i-2, i+3)
+	//                   | - (i-3, i+4)
+	//
+	//newHtml = command.substr(0, cursorPosition[0]) + '<span class="onCursor">' + command.substr(cursorPosition[0], 1)+ '</span>' + command.substr(cursorPosition[0] +1);
+	newHtml = commandBeforeCurs + '<span class="onCursor">' + commandInCursor + '</span>' + commandAfterCurs;
 
 	$('.active_command').html(newHtml);
+}
+
+function fuckCharacter(command, start) {
+	if (/&lt;/.test(command.substr(cursorPosition[0], 3)) {
+		return true;
+	}
 }
 
 
@@ -226,13 +250,13 @@ function parseCommand(command) {
 
 function execHelp() {
 	var helpText = "You want help? \
-		<br> \
-		<br> \
-		help: list commands<br> \
-		clear: clear the terminal screen<br> \
-		cat: Concatenate FILE(s)<br> \
-		history: display the command history list with line numbers<br> \
-		hostname: display hostname or set hostname<br> \
+		\n \
+		\n \
+		help: list commands\n \
+		clear: clear the terminal screen\n \
+		cat: Concatenate FILE(s)\n \
+		history: display the command history list with line numbers\n \
+		hostname: display hostname or set hostname\n \
 		pwd: print name of current/working directory";
 
 	exitStatus = 0;
@@ -332,7 +356,7 @@ function execHistory(args) {
 	historyOutput = "";
 
 	commandHistory.forEach(function(element, index) {
-		historyOutput += index + " : " + element + "<br>";
+		historyOutput += index + " : " + element + "\n";
 	});
 
 	exitStatus = 0;
